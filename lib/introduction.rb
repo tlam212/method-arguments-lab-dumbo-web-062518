0567bb835918d1bebf1ab1f717d4440abcbaf3ac
Toni Lamont <tlam212@gmail.com>
2018-06-11 18:20:53 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 def introduction(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
 introduction("Dan", "Ember")
